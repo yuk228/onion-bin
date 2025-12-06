e672d4c59fcc8d003d74fcc673f96da5badae33e
yuk228 <194586153+yuk228@users.noreply.github.com>
2025-12-07 01:20:28 +0900

    ./frontendからrootにファルを移動しました

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Textarea } from '@/components/ui/textarea'
+
+export default function Page() {
+    return (
+        <main className="mt-20 w-full px-5">
+            <Textarea
+                placeholder="Enter text here"
+                className="min-h-[400px] w-full"
+            />
+            
+        </main>
+    )
+}
